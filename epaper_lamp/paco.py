56b12444947c70f3695909a9bc8f02a5b4fc6d91
itspacchu <37984032+itspacchu@users.noreply.github.com>
2022-08-03 16:52:11 +0530

    remove red drawing

--- epaper_lamp/paco.py
+++ epaper_lamp/paco.py
@@ -1,9 +1,16 @@
 #!/usr/bin/python3
 import os,subprocess
-from PIL import Image
+from PIL import Image,ImageEnhance
 from time import sleep
 
 SLEEP_TIME = 30
+
+def change_contrast(img, level):
+    factor = (259 * (level + 255)) / (255 * (259 - level))
+    def contrast(c):
+        return 128 + factor * (c - 128)
+    return img.point(contrast)
+
 
 def epd(blackimg:str="./images/null.bmp",redimg:str="./images/null.bmp"):
     subprocess.run(["sudo","./epd", blackimg, redimg])
@@ -12,10 +19,11 @@ def render_webpage(url:str="https://smartcitylivinglab.iiit.ac.in/home/"):
     #chrome --headless --disable-gpu --screenshot --window-size=1280,1696 https://www.chromestatus.com/
     subprocess.run(['chromium','--headless','--disable-gpu','--screenshot','--window-size=480,800',url])
     webpage = Image.open("screenshot.png").convert("RGB").transpose(Image.ROTATE_90)
-    r = webpage.split()[0]
-    r.point( lambda p: 255 - p if ((p > 100) and (p < 240)) else 255 ).convert("1").save("screenshot_r.bmp")
+    webpage = change_contrast(webpage,100)
+    #r = webpage.split()[0]
+    #r.point( lambda p: 255 - (10*p) if ((p >= 200) and (p < 240)) else 255 ).convert("1").save("screenshot_r.bmp")
     webpage.convert("1").save("screenshot_b.bmp")
-    epd("./screenshot_b.bmp","./screenshot_r.bmp")
+    epd("./screenshot_b.bmp")
 
 if(__name__ == "__main__"):
     while(True):
